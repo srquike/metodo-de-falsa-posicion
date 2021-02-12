e77bdb2df43652b8505366dca83a582eec02487c
srquike <srquike@outlook.com>
2021-02-12 17:38:56 -0600

    Agregar archivos de proyecto.

---
+++ Iteracion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodoDeFalsaPosicion
+{
+    public class Iteracion
+    {
+        public int I { get; set; }
+        public double A { get; set; }
+        public double B { get; set; }
+        public double Fa { get; set; }
+        public double Fb { get; set; }
+        public double Xr { get; set; }
+        public double Fxr { get; set; }
+        public double FaFxr { get; set; }
+        public double Error { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetodoDeFalsaPosicion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool AreControlsValid()
+        {
+            if (double.TryParse(TxtIntervaloA.Text, out double a) && !string.IsNullOrEmpty(TxtIntervaloA.Text))
+            {
+                if (double.TryParse(TxtIntervaloB.Text, out double b) && !string.IsNullOrEmpty(TxtIntervaloB.Text))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void BtnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            if (AreControlsValid())
+            {
+                double a, b;
+                int i;
+                string error, err;
+                Iteracion iteracionAnterior, iteracion;
+                IList<Iteracion> iteraciones;
+
+                error = TxtError.Text;
+                iteraciones = new List<Iteracion>();
+                iteracionAnterior = new Iteracion();
+                a = double.Parse(TxtIntervaloA.Text);
+                b = double.Parse(TxtIntervaloB.Text);
+                i = 0;
+
+                do
+                {
+                    iteracion = new Iteracion();
+                    iteracion.I = i;
+
+                    if (iteracion.I == 0)
+                    {
+                        iteracion.A = a;
+                        iteracion.B = b;
+                        iteracion.Fa = Math.Sin(iteracion.A) + (2 * iteracion.A) - 1;
+                        iteracion.Fb = Math.Sin(iteracion.B) + (2 * iteracion.B) - 1;
+                        iteracion.Xr = iteracion.B - (iteracion.Fb * (iteracion.A - iteracion.B) / (iteracion.Fa - iteracion.Fb));
+                        iteracion.Error = -1;
+                    }
+                    else
+                    {
+                        if (iteracionAnterior.FaFxr < 0)
+                        {
+                            iteracion.A = iteracionAnterior.A;
+                            iteracion.B = iteracionAnterior.Xr;
+                        }
+                        else if (iteracionAnterior.FaFxr > 0)
+                        {
+                            iteracion.A = iteracionAnterior.Xr;
+                            iteracion.B = iteracionAnterior.B;
+                        }
+
+                        iteracion.Fa = Math.Sin(iteracion.A) + (2 * iteracion.A) - 1;
+                        iteracion.Fb = Math.Sin(iteracion.B) + (2 * iteracion.B) - 1;
+                        iteracion.Xr = iteracion.B - (iteracion.Fb * (iteracion.A - iteracion.B) / (iteracion.Fa - iteracion.Fb));
+                        iteracion.Error = Math.Abs(100 * (iteracion.Xr - iteracionAnterior.Xr) / iteracion.Xr);
+                    }
+
+                    iteracion.Fxr = Math.Sin(iteracion.Xr) + (2 * iteracion.Xr) - 1;
+                    iteracion.FaFxr = iteracion.Fa * iteracion.Fxr;
+
+                    iteracionAnterior = iteracion;
+
+                    i++;
+
+                    iteraciones.Add(iteracion);
+
+                    err = string.Format("{0:N3}", iteracion.Error);
+
+                } while (!err.Equals(error) && !err.Equals("0.000"));
+
+                DgIteraciones.ItemsSource = iteraciones;
+            }
+            else
+            {
+                MessageBox.Show("ALGUNOS VALORES INGRESADOR POR EL USUARIO SON INCORRECTOS", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            TxtIntervaloA.Clear();
+            TxtIntervaloB.Clear();
+            DgIteraciones.ItemsSource = null;
+            TxtIntervaloA.Focus();
+        }
+    }
+}
